12df6c9bea78f1014c18ab58b9e05bc9024b4d56
Belle Aerni <belleaerni@gmail.com>
2023-04-30 00:57:56 -0700

    Move Rector config

--- rector.php
+++ rector.php
@@ -7,13 +7,12 @@ use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/',
+        __DIR__ . '/src',
     ]);
 
     $rectorConfig->skip([
-        __DIR__ . '/vendor',
-        __DIR__ . '/data',
-        __DIR__ . '/library',
+        __DIR__ . '/src/vendor',
+        __DIR__ . '/src/data',
         UnionTypesRector::class,
         MixedTypeRector::class,
     ]);
